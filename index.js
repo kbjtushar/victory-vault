4fabfbf75fbbc386d0787051a8cabe17450b3f91
kbjtushar <161484157+kbjtushar@users.noreply.github.com>
2024-03-01 16:00:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*! yt-player. MIT License. Feross Aboukhadijeh <https://feross.org/opensource> */
+
 var EventEmitter = function () {
     this.events = {};
   };
@@ -164,8 +164,7 @@ var EventEmitter = function () {
         if (err) return this._destroy(new Error('YouTube Iframe API failed to load'))
         this._api = api
   
-        // If load(videoId, [autoplay, [size]]) was called before Iframe API
-        // loaded, ensure it gets called again now
+        
         if (this.videoId) this.load(this.videoId, this._autoplay, this._start)
       })
     }
@@ -195,22 +194,17 @@ var EventEmitter = function () {
       this._autoplay = autoplay
       this._start = start
   
-      // If the Iframe API is not ready yet, do nothing. Once the Iframe API is
-      // ready, `load(this.videoId)` will be called.
+      
       if (!this._api) return
   
-      // If there is no player instance, create one.
       if (!this._player) {
         this._createPlayer(videoId)
         return
       }
   
-      // If the player instance is not ready yet, do nothing. Once the player
-      // instance is ready, `load(this.videoId)` will be called. This ensures that
-      // the last call to `load()` is the one that takes effect.
+      
       if (!this._ready) return
   
-      // If the player instance is ready, load the given `videoId`.
       if (autoplay) {
         this._player.loadVideoById(videoId, start)
       } else {
@@ -453,15 +447,13 @@ var EventEmitter = function () {
         return cb(null, window.YT)
       }
   
-      // Otherwise, queue callback until API is loaded
+      
       loadIframeAPICallbacks.push(cb)
   
       var scripts = Array.from(document.getElementsByTagName('script'))
       var isLoading = scripts.some(script => script.src === YOUTUBE_IFRAME_API_SRC)
   
-      // If API <script> tag is not present in the page, inject it. Ensures that
-      // if user includes a hardcoded <script> tag in HTML for performance, another
-      // one will not be added
+      
       if (!isLoading) {
         loadScript(YOUTUBE_IFRAME_API_SRC).catch(err => {
           while (loadIframeAPICallbacks.length) {
@@ -493,117 +485,53 @@ var EventEmitter = function () {
         height: opts.height,
         videoId: videoId,
   
-        // (Not part of documented API) This parameter controls the hostname that
-        // videos are loaded from. Set to `'https://www.youtube-nocookie.com'`
-        // for enhanced privacy.
+        
         host: opts.host,
   
         playerVars: {
-          // This parameter specifies whether the initial video will automatically
-          // start to play when the player loads. Supported values are 0 or 1. The
-          // default value is 0.
+          
           autoplay: opts.autoplay ? 1 : 0,
 
           mute: opts.mute ? 1 : 0,
   
-          // Setting the parameter's value to 1 causes closed captions to be shown
-          // by default, even if the user has turned captions off. The default
-          // behavior is based on user preference.
-          // cc_load_policy: opts.captions != null
-          //   ? opts.captions !== false ? 1 : 0
-          //   : undefined, // default to not setting this option
-  
-          // Sets the player's interface language. The parameter value is an ISO
-          // 639-1 two-letter language code or a fully specified locale. For
-          // example, fr and fr-ca are both valid values. Other language input
-          // codes, such as IETF language tags (BCP 47) might also be handled
-          // properly.
+          
           hl: (opts.captions != null && opts.captions !== false)
             ? opts.captions
-            : undefined, // default to not setting this option
+            : undefined, 
   
-          // This parameter specifies the default language that the player will
-          // use to display captions. Set the parameter's value to an ISO 639-1
-          // two-letter language code.
+          
           cc_lang_pref: (opts.captions != null && opts.captions !== false)
             ? opts.captions
-            : undefined, // default to not setting this option
+            : undefined, 
   
-          // This parameter indicates whether the video player controls are
-          // displayed. For IFrame embeds that load a Flash player, it also defines
-          // when the controls display in the player as well as when the player
-          // will load. Supported values are:
-          //   - controls=0 – Player controls do not display in the player. For
-          //                  IFrame embeds, the Flash player loads immediately.
-          //   - controls=1 – (default) Player controls display in the player. For
-          //                  IFrame embeds, the controls display immediately and
-          //                  the Flash player also loads immediately.
-          //   - controls=2 – Player controls display in the player. For IFrame
-          //                  embeds, the controls display and the Flash player
-          //                  loads after the user initiates the video playback.
+          
           controls: opts.controls ? 2 : 0,
   
-          // Setting the parameter's value to 1 causes the player to not respond to
-          // keyboard controls. The default value is 0, which means that keyboard
-          // controls are enabled.
-          // disablekb: opts.keyboard ? 0 : 1,
-  
-          // Setting the parameter's value to 1 enables the player to be
-          // controlled via IFrame or JavaScript Player API calls. The default
-          // value is 0, which means that the player cannot be controlled using
-          // those APIs.
+          
           enablejsapi: 1,
   
-          // Setting this parameter to 0 prevents the fullscreen button from
-          // displaying in the player. The default value is 1, which causes the
-          // fullscreen button to display.
+          
           allowfullscreen: true,
   
-          // Setting the parameter's value to 1 causes video annotations to be
-          // shown by default, whereas setting to 3 causes video annotations to not
-          // be shown by default. The default value is 1.
+          
           iv_load_policy: opts.annotations ? 1 : 3,
   
-          // This parameter lets you use a YouTube player that does not show a
-          // YouTube logo. Set the parameter value to 1 to prevent the YouTube logo
-          // from displaying in the control bar. Note that a small YouTube text
-          // label will still display in the upper-right corner of a paused video
-          // when the user's mouse pointer hovers over the player.
+          
           modestbranding: opts.modestBranding ? 1 : 0,
   
-          // This parameter provides an extra security measure for the IFrame API
-          // and is only supported for IFrame embeds. If you are using the IFrame
-          // API, which means you are setting the enablejsapi parameter value to 1,
-          // you should always specify your domain as the origin parameter value.
+          
           origin: '*',
   
-          // This parameter controls whether videos play inline or fullscreen in an
-          // HTML5 player on iOS. Valid values are:
-          //   - 0: This value causes fullscreen playback. This is currently the
-          //        default value, though the default is subject to change.
-          //   - 1: This value causes inline playback for UIWebViews created with
-          //        the allowsInlineMediaPlayback property set to TRUE.
-          // playsinline: opts.playsInline ? 1 : 0,
-  
-          // This parameter indicates whether the player should show related
-          // videos from the same channel (0) or from any channel (1) when
-          // playback of the video ends. The default value is 1.
+          
           rel: opts.related ? 1 : 0,
   
-          // (Not part of documented API) Allow html elements with higher z-index
-          // to be shown on top of the YouTube player.
+          
           mode: 'transparent',
           showinfo: 0,
           html5: 1,
           version: 3,
           playerapiid: 'iframe_YTP_1624972482514'
-          // version=3&playerapiid=iframe_YTP_1624972482514
-          // This parameter causes the player to begin playing the video at the given number
-          // of seconds from the start of the video. The parameter value is a positive integer.
-          // Note that similar to the seek function, the player will look for the closest
-          // keyframe to the time you specify. This means that sometimes the play head may seek
-          // to just before the requested time, usually no more than around two seconds.
-          // start: opts.start
+          
         },
         events: {
           onReady: () => this._onReady(videoId),
@@ -615,89 +543,58 @@ var EventEmitter = function () {
       })
     }
   
-    /**
-     * This event fires when the player has finished loading and is ready to begin
-     * receiving API calls.
-     */
+    
     _onReady (videoId) {
       if (this.destroyed) return
       
       this._ready = true
 
-      // Once the player is ready, always call `load(videoId, [autoplay, [size]])`
-      // to handle these possible cases:
-      //
-      //   1. `load(videoId, true)` was called before the player was ready. Ensure that
-      //      the selected video starts to play.
-      //
-      //   2. `load(videoId, false)` was called before the player was ready. Now the
-      //      player is ready and there's nothing to do.
-      //
-      //   3. `load(videoId, [autoplay])` was called multiple times before the player
-      //      was ready. Therefore, the player was initialized with the wrong videoId,
-      //      so load the latest videoId and potentially autoplay it.
+      
       this.load(this.videoId, this._autoplay, this._start)
   
       this._flushQueue()
     }
   
-    /**
-     * Called when the player's state changes. We emit friendly events so the user
-     * doesn't need to use YouTube's YT.PlayerState.* event constants.
-     */
+    
     _onStateChange (data) {
       if (this.destroyed) return
   
       var state = YOUTUBE_STATES[data.data]
   
       if (state) {
-        // Send a 'timeupdate' anytime the state changes. When the video halts for any
-        // reason ('paused', 'buffering', or 'ended') no further 'timeupdate' events
-        // should fire until the video unhalts.
+        
         if (['paused', 'buffering', 'ended'].includes(state)) this._onTimeupdate()
   
         this.emit(state)
   
-        // When the video changes ('unstarted' or 'cued') or starts ('playing') then a
-        // 'timeupdate' should follow afterwards (never before!) to reset the time.
         if (['unstarted', 'playing', 'cued'].includes(state)) this._onTimeupdate()
       } else {
         throw new Error('Unrecognized state change: ' + data)
       }
     }
   
-    /**
-     * This event fires whenever the video playback quality changes. Possible
-     * values are: 'small', 'medium', 'large', 'hd720', 'hd1080', 'highres'.
-     */
+    
     _onPlaybackQualityChange (data) {
       if (this.destroyed) return
       this.emit('playbackQualityChange', data.data)
     }
   
-    /**
-     * This event fires whenever the video playback rate changes.
-     */
+    
     _onPlaybackRateChange (data) {
       if (this.destroyed) return
       this.emit('playbackRateChange', data.data)
     }
   
-    /**
-     * This event fires if an error occurs in the player.
-     */
+    
     _onError (data) {
       if (this.destroyed) return
   
       var code = data.data
   
-      // The HTML5_ERROR error occurs when the YouTube player needs to switch from
-      // HTML5 to Flash to show an ad. Ignore it.
+      
       if (code === YOUTUBE_ERROR.HTML5_ERROR) return
   
-      // The remaining error types occur when the YouTube player cannot play the
-      // given video. This is not a fatal error. Report it as unplayable so the user
-      // has an opportunity to play another video.
+      
       if (code === YOUTUBE_ERROR.UNPLAYABLE_1 ||
           code === YOUTUBE_ERROR.UNPLAYABLE_2 ||
           code === YOUTUBE_ERROR.NOT_FOUND ||
@@ -705,7 +602,7 @@ var EventEmitter = function () {
         return this.emit('unplayable', this.videoId)
       }
   
-      // Unexpected error, does not match any known type
+      
       this._destroy(new Error('YouTube Player Error. Unknown error code: ' + code))
     }
 
@@ -715,10 +612,7 @@ var EventEmitter = function () {
       window.addEventListener('resize', this._resizeListener);
     }
 
-    /**
-     * This event fires when the time indicated by the `getCurrentTime()` method
-     * has been updated.
-     */
+    
     _onTimeupdate () {
       this.emit('timeupdate', this.getCurrentTime())
     }
